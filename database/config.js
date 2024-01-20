c4f90122a9025806e44dd9cdd3dc868da00b35fb
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2024-01-20 12:40:49 -0300

    mongoose update config

--- database/config.js
+++ database/config.js
@@ -5,7 +5,11 @@ mongoose.set('strictQuery', false);
 
 export const dbConnection = async()=>{
     try {
-        await mongoose.connect(process.env.DB_URL)
+        await mongoose.connect(process.env.DB_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        })
         console.log("Database is on air");
     } catch (error) {
         console.log(error)
